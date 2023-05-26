eeb7dceb8350795ce8154b27333db607f2281a13
mohammaddargahi <mohammaddargahi4@gmail.com>
2023-05-26 18:14:50 +0330

    first commit

---
+++ repositories/users.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const crypto = require("crypto");
+const util = require("util");
+const Repository = require("./repository");
+const scrypt = util.promisify(crypto.scrypt);
+
+class UserRepository extends Repository {
+  async comparePasswords(saved, supplied) {
+    //saved -> password saved in our database. 'hashed.salt'
+    //Supplied -> password given to us by a user trying sign in
+    const [hashed, salt] = saved.split(".");
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuf.toString("hex");
+  }
+
+  async create(attrs) {
+    //attrs === {email: '' , password: ''}
+    attrs.id = this.randomId();
+
+    const salt = crypto.randomBytes(8).toString("hex");
+    const buf = await scrypt(attrs.password, salt, 64);
+
+    const records = await this.getAll();
+    const record = {
+      ...attrs,
+      password: `${buf.toString("hex")}.${salt}`,
+    };
+    records.push(record);
+
+    await this.writeAll(records);
+    return record;
+  }
+}
+
+module.exports = new UserRepository("users.json");
---
+++ routes/products.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const productsRepo = require("../repositories/products");
+const productIndexTemplate = require("../views/products/index.js");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const products = await productsRepo.getAll();
+
+  res.send(productIndexTemplate({ products }));
+});
+
+module.exports = router;
